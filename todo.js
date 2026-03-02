b11773db0cd9e718f6841cd0d2529b11154f2844
Baby-molato <aniseremolato@gmail.com>
2026-03-02 04:35:43 -0800

    js add now

---
+++ todo.js
@@ -0,0 +1,20 @@
+const eventInput = document.getElementById("event");
+const eventsHeader = document.getElementById("eventsHeader");
+const eventsList = document.getElementById("eventsList");
+const addEventButton = document.getElementById("addEventButton");
+const checkbox = document.getElementById("event1");
+const checkboxText = document.querySelector(".checkboxText");
+
+   
+function checkboxClicked(checkbox) {
+    const checkboxText = checkbox.nextElementSibling;
+    if (checkbox.checked) {
+        checkboxText.classList.add("text-gray-400","line-through");
+    } else {
+        checkboxText.classList.remove("text-gray-400","line-through");
+    }
+}
+
+checkbox.addEventListener("change", function() {
+    checkboxClicked(this);
+});
